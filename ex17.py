8dcf86dd0ba379d1fbe72565520186b346516658
sdzephyrx <sdzephyrx@gmail.com>
2020-01-13 08:25:56 -0300

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -1,14 +1,19 @@
 from sys import argv
+#import do comando exists da biblioteca os.path
 from os.path import exists
 
 script, from_file, to_file= argv
 
-print("Copiando de {from_file} para {to_file}.")
+print(f"Copiando de {from_file} para {to_file}.")
+#abrindo e armazenando o arquivo
 in_file= open(from_file)
+#lendo o conteudo e armazenando
 in_data= in_file.read()
 
-print("O arquivo {from_file} tem {len(indata)} bytes.")
-print("O arquivo {to_file} existe? {exists(to_file)}")
+#O comando len() conta o tamanho em bytes
+print(f"O arquivo {from_file} tem {len(in_data)} bytes.")
+#o comando exists() realiza o teste se existe ou nao
+print(f"O arquivo {to_file} existe? {exists(to_file)}")
 print("Abort ? CTRL+C (sim), ENTER (não)")
 input("? ")
 
